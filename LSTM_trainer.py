13785edeb7da9b9bc93a7c56eb1cacbb01c693b9
hoang-ho <hazardhoang2610@gmail.com>
2020-05-03 13:56:12 -0400

    Attn LSTM

--- LSTM_trainer.py
+++ LSTM_trainer.py
@@ -7,7 +7,6 @@ from torch import optim
 import numpy as np
 import time
 from livelossplot import PlotLosses
-from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import random
 from transformers import AlbertModel, AlbertTokenizer, AdamW
@@ -27,8 +26,7 @@ def flat_accuracy(preds, labels):
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
 
 
-
-def trainer(classifier, optimizer, scheduler, epochs, early_stop, train_dataloader, validation_dataloader, seed_val=0, accumulation_steps=1):
+def trainer(classifier, optimizer, scheduler, epochs, early_stop, train_dataloader, validation_dataloader, save_file, seed_val=0, accumulation_steps=1):
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         classifier = nn.DataParallel(classifier)
@@ -44,11 +42,11 @@ def trainer(classifier, optimizer, scheduler, epochs, early_stop, train_dataload
     torch.cuda.manual_seed_all(seed_val)
     best = (np.inf, -1, -np.inf, None, None)
 
-    # liveloss = PlotLosses()
-    # LossHistory = []
-    writer = SummaryWriter("runs/AttnLSTM")
+    liveloss = PlotLosses()
+    LossHistory = []
     val_step = 0
     for epoch_i in range(0, epochs):
+        logs = {}
         print("")
         print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
         print("Global Learning Rate", optimizer.param_groups[0]["lr"])
@@ -58,7 +56,6 @@ def trainer(classifier, optimizer, scheduler, epochs, early_stop, train_dataload
         start = time.time()
         classifier.zero_grad()
         for step, batch in enumerate(train_dataloader):
-            logs = {}
             b_inputs_c = batch[0].to(device)
             b_inputs_r = batch[1].to(device)
             b_mask_c = batch[2].to(device)
@@ -78,70 +75,75 @@ def trainer(classifier, optimizer, scheduler, epochs, early_stop, train_dataload
                 optimizer.step()
                 scheduler.step()
                 classifier.zero_grad()
-                
+            
             batch_loss = loss.cpu().item()
             epoch_loss += loss.cpu().item()
 
-            
-            if (step % 1000 == 0 and step > 0) or (step == len(train_dataloader) - 1):
-                print("Step %i with loss %f elapsed time %f" % (step, batch_loss, time.time() - start))
-                writer.add_scalar("Loss/train", batch_loss, step)
-                print('Evaluating...')
-                classifier.eval()
-                dev_loss = 0.
-                total_eval_accuracy = 0.
-                y_preds = None
-                y_true = None
-                for batch in validation_dataloader:
-                    b_inputs_c = batch[0].to(device)
-                    b_inputs_r = batch[1].to(device)
-                    b_mask_c = batch[2].to(device)
-                    b_mask_r = batch[3].to(device)
-                    b_labels = batch[4].to(device)
-                        
-                    with torch.no_grad():
-                        x_c = embedder(input_ids=b_inputs_c, attention_mask=b_mask_c)[0]
-                        x_r = embedder(input_ids=b_inputs_r, attention_mask=b_mask_r)[0]
-                        loss, logits = classifier(x_c.permute(1,0,2), x_r.permute(1,0,2), b_labels)
-                        if torch.cuda.device_count() > 1:
-                            loss = loss.sum()
-                            
-                    dev_loss += loss.cpu().item()
-                    label_ids = b_labels.cpu().numpy()
-                    logits = logits.detach().cpu().numpy()
-                    total_eval_accuracy += flat_accuracy(logits, label_ids)           
-                    if y_preds is None:
-                        y_preds = np.argmax(logits, axis=1)
-                        y_true = label_ids
-                    else:
-                        y_preds = np.concatenate((y_preds, np.argmax(logits, axis=1)))
-                        y_true = np.concatenate((y_true, label_ids))
-                
-                avg_val_accuracy = total_eval_accuracy / len(validation_dataloader)
-                writer.add_scalar('Loss/test', dev_loss, step)
-                writer.add_scalar("Accuracy/Test", avg_val_accuracy, step)
-                f1_score_1 = precision_recall_fscore_support(y_true, y_preds, average="binary")
-                f1_score_0 = precision_recall_fscore_support(y_true, y_preds, average="binary", pos_label=0)
-
-                print("Epoch %i with dev loss %f and dev accuracy %f" % (epoch_i + 1, dev_loss, avg_val_accuracy))
-
-                if(val_step-best[1] >= early_stop and best[0] < dev_loss):
-                    print("early_stopping, epoch:", epoch_i+1)
-                    print("Final dev loss %f Final Train Loss %f Final dev accuracy %f" % (dev_loss, epoch_loss, avg_val_accuracy))
-                    print("Best dev loss %f Best dev accuracy %f" % (best[0], best[2]))
-                    print("F1_score Sarcasm ", f1_score_1)
-                    print("F1_score Non-Sarcasm ", f1_score_0)
-
-                    return classifier
-                elif(best[0] > dev_loss):
-                    best = (dev_loss, val_step, avg_val_accuracy, f1_score_1, f1_score_0)
-                    torch.save(classifier.state_dict(), 'checkpoint_big.pt')
-                    
-                val_step += 1
-
-            elif (step % 100) == 0:
+            if (step % 100) == 0:
                 print("Step %i with loss %.3f elapsed time %.3f" % (step, batch_loss, time.time() - start))
-                writer.add_scalar("Loss/train", batch_loss, step)
+                # writer.add_scalar("Loss/train", epoch_loss/(step+1), global_step)
+                # writer.flush()
+            
+        print("Step %i with loss %f elapsed time %f" % (step, batch_loss, time.time() - start))
+        print('Evaluating...')
+        classifier.eval()
+        dev_loss = 0.
+        total_eval_accuracy = 0.
+        y_preds = None
+        y_true = None
+        for batch in validation_dataloader:
+            b_inputs_c = batch[0].to(device)
+            b_inputs_r = batch[1].to(device)
+            b_mask_c = batch[2].to(device)
+            b_mask_r = batch[3].to(device)
+            b_labels = batch[4].to(device)
+                
+            with torch.no_grad():
+                x_c = embedder(input_ids=b_inputs_c, attention_mask=b_mask_c)[0]
+                x_r = embedder(input_ids=b_inputs_r, attention_mask=b_mask_r)[0]
+                loss, logits = classifier(x_c.permute(1,0,2), x_r.permute(1,0,2), b_labels)
+                if torch.cuda.device_count() > 1:
+                    loss = loss.sum()
+                    
+            dev_loss += loss.cpu().item()
+            label_ids = b_labels.cpu().numpy()
+            logits = logits.detach().cpu().numpy()
+            total_eval_accuracy += flat_accuracy(logits, label_ids)           
+            if y_preds is None:
+                y_preds = np.argmax(logits, axis=1)
+                y_true = label_ids
+            else:
+                y_preds = np.concatenate((y_preds, np.argmax(logits, axis=1)))
+                y_true = np.concatenate((y_true, label_ids))
+        
+        avg_val_accuracy = total_eval_accuracy / len(validation_dataloader)
+        f1_score_1 = precision_recall_fscore_support(y_true, y_preds, average="binary")
+        f1_score_0 = precision_recall_fscore_support(y_true, y_preds, average="binary", pos_label=0)
+
+        print("Epoch %i with dev loss %f and dev accuracy %f" % (epoch_i + 1, dev_loss, avg_val_accuracy))
+
+        logs["val_loss"] = dev_loss / len(validation_dataloader)
+        logs["loss"] = epoch_loss / len(train_dataloader)
+        logs["val_accuracy"] = avg_val_accuracy
+        liveloss.update(logs)
+        LossHistory.append(logs["loss"])
+        liveloss.send()
+
+        if(val_step-best[1] >= early_stop and best[0] < dev_loss):
+            print("early_stopping, epoch:", epoch_i+1)
+            print("Final dev loss %f Final Train Loss %f Final dev accuracy %f" % (dev_loss, epoch_loss, avg_val_accuracy))
+            print("Best dev loss %f Best dev accuracy %f" % (best[0], best[2]))
+            print("F1_score Sarcasm ", f1_score_1)
+            print("F1_score Non-Sarcasm ", f1_score_0)
+
+            return classifier
+        elif(best[0] > dev_loss):
+            best = (dev_loss, val_step, avg_val_accuracy, f1_score_1, f1_score_0)
+            torch.save(classifier.state_dict(), save_file)
+            
+        val_step += 1
+        classifier.train()
+
         
     print("Final dev loss %f Final Train Loss %f Final dev accuracy %f" % (dev_loss, epoch_loss, avg_val_accuracy))
     print("Best dev loss %f Best dev accuracy %f" % (best[0], best[2]))
---
+++ models/conditionalLSTM.py
@@ -0,0 +1,48 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+
+class AttnLSTM(nn.Module):
+    def __init__(self, embedding_dim=768, hidden_dim=128, num_layers=1, label_size=2):
+        super(AttnLSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(input_size=embedding_dim, num_layers=num_layers,
+                            hidden_size=hidden_dim, bidirectional=True)
+        self.attnLinear = nn.Linear(hidden_dim*2, hidden_dim)
+        self.context = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x,  return_att=False):
+        # x : n, B, d
+        lstm_out, (h_n, c_n) = self.lstm(x)   # (n, B, 2h), ((2, B, h), (2, B, h))
+        attn = torch.tanh(self.attnLinear(lstm_out))  # n, B, h
+        score = F.softmax(self.context(attn), dim=0)  # n, B, 1
+        lstm_out = lstm_out.permute(1, 2, 0)  # B, 2h, n
+        score = score.permute(1, 0, 2)  # B, n, 1
+        a = torch.bmm(lstm_out, score).squeeze(dim=2)  # B, 2h
+
+        return a, (h_n, c_n)
+
+class ConditionalEncoding(nn.Module):
+    def __init__(self, embedding_dim=768, hidden_dim=128, num_layers=1, label_size=2):
+        super(AttnLSTM, self).__init__()
+        self.LSTM_c = AttnLSTM(embedding_dim, hidden_dim, num_layers, label_size)
+        self.LSTM_r = AttnLSTM(embedding_dim, hidden_dim, num_layers, label_size)
+        self.hidden2label = nn.Linear(hidden_dim*4, label_size)
+        self.loss_function = nn.CrossEntropyLoss()
+    
+    def forward(self, x_c, x_r, y_true, return_att=False):
+        batch_size = x_r.size(1)
+        num_directions = 2 
+        hr_0 = torch.zeros(self.num_layers * num_directions, batch_size, self.hidden_size,
+                            dtype=x_r.dtype, device=x_r.device)
+
+        v_c, (hn_c, cn_c) = self.LSTM_c(x_c)
+        
+        v_r = self.LSTM_r(x_r, (hr_0, cn_c))
+        v = torch.cat((v_c, v_r), dim=1) # B, 4h
+        logits = self.hidden2label(v)
+        loss = self.loss_function(logits, y_true)
+
+        return loss, logits
